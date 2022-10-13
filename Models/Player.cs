ea6e55f8cde1f46a0d56c9c32d73783109ed18cb
Kaydon stubbs <k.rydge.stubbs@gmail.com>
2022-10-13 15:33:40 -0600

    inital creation of monogame project

---
+++ Models/Player.cs
@@ -0,0 +1,21 @@
+namespace Player
+{
+    class Player
+    {
+        private int xpos;
+        private int xsize;
+        private int ypos;
+        private int ysize;
+        private int xVelocity;
+        private int yVelocity;
+        public Player()
+        {
+            xsize= 20;
+            ysize= 35;
+            xpos= 0;
+            ypos=0;
+            xVelocity=0;
+            yVelocity= 0;
+        }
+    }
+}
